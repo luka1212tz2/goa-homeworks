94cf5590b672393fbd38801920e9730baa0ae3c0
luka1212tz2 <15nlluksboy@gmail.com>
2024-06-02 09:55:04 +0400

    sixth commit

---
+++ level 6/homework/hw.py
@@ -0,0 +1,16 @@
+name = "data"
+surname = "diasamidze"
+age = 100
+text = "I am programmer"
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
+
+
+
+
+name = "luka"
+surname = "modebadze"
+age = 12
+text = "i am learing programming in goa"
+
+print("My name is",  name,  "my surname is", surname, "my age is", age, text)

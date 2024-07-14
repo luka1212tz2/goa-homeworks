77ed68c4c641646cee890ec25f35b57db3158e65
luka1212tz2 <168012266+luka1212tz2@users.noreply.github.com>
2024-07-14 10:22:02 +0400

    Add files via upload

---
+++ level 16/classwork/cw.py
@@ -0,0 +1,46 @@
+num1 = 3.14
+num2 = 3.149
+print(num1 + num2)
+
+name = "luka"
+surname = "modebadze"
+print(name + surname)
+
+age = int(input("your age:"))
+print("your age after 10 years old", age + 10 )
+
+print(23 == 21)
+print(13 != 21)
+print(83 < 21)
+print(83 > 72)
+print(83 <= 92)
+print(33 >= 21)
+print(39 == 31)
+print(83 != 21)
+print(81 < 41)
+print(93 > 62)
+
+True and False 
+True and True
+False and False 
+False and True
+True or False 
+True or True
+False or False  
+False or True
+True or True
+False and False
+
+print(57 < 4 and 35 > 42)
+print(33 < 11 and 15 > 31)
+print(39 < 28 and 71 > 9)
+print(30 < 29 and 13 > 49)
+print(49 < 17 and 25 > 17)
+print(41 < 11 or 13 > 17)
+print(49 < 13 or 15 > 41)
+print(10 < 29 or 15 > 2)
+print(17 < 62 or 15 > 2)
+print(10 < 41 or 49 > 2)
+
+#pseudocode არის კოდი რომელიც არ არის განკუთვნილი პროგრამირების ენებისთვის, ადამიანებს ეხმარებათ pseudocode რომ გაიგონ რა წერია კოდში ცოდნის გარეშე
+#selection არის რაღაცის არჩევა, მაგალითად შენ უნდა აირჩიო ერთი ქვეყანა სადაც იმოგზაურებ 1 კვირა 
